0dfb87db592f316b6a33ab37c1563ec9da1ac4f4
WincyLiu <87019971+WincyLiu@users.noreply.github.com>
2021-08-16 15:53:21 +0100

    Added Search Engine and Geolocation

--- src/index.js
+++ src/index.js
@@ -44,3 +44,65 @@ function formatDate() {
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
+
+//Search
+function searchCity(city) {
+  let apiKey = "f3a6803962b9a95d77dfdbc8dc0f3991";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function submitCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+searchCity("London");
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", submitCity);
+
+function showTemperature(response) {
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+
+  celsiusTemperature = response.data.main.temp;
+
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(celsiusTemperature);
+
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].main;
+
+  let currentHumidity = document.querySelector("#humidity");
+  let humidity = response.data.main.humidity;
+  currentHumidity.innerHTML = `Humidity: ${humidity}%`;
+
+  let windSpeed = document.querySelector("#wind");
+  let speed = Math.round(response.data.wind.speed);
+  windSpeed.innerHTML = `Wind Speed: ${speed} m/s`;
+
+  let cloud = document.querySelector("#cloudiness");
+  let cloudy = response.data.clouds.all;
+  cloud.innerHTML = `Cloudiness: ${cloudy}%`;
+}
+
+//Geolocation
+function currentLocation(position) {
+  let longitude = position.coords.longitude;
+  let latitude = position.coords.latitude;
+
+  let apiKey = "f3a6803962b9a95d77dfdbc8dc0f3991";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function findLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentLocation);
+}
+
+let geolocationButton = document.querySelector("#geolocation");
+geolocationButton.addEventListener("click", findLocation);
